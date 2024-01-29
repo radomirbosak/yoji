25904db65b2b21f8f6d7f942a549644822e854c3
Radomír Bosák <radomir.bosak@gmail.com>
2024-01-29 20:28:04 +0900

    Add kanji and kana methods

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,21 @@ impl fmt::Display for Part {
 #[derive(Default, Debug, PartialEq)]
 struct Yojijukugo(Part, Part, Part, Part);
 
+impl Yojijukugo {
+    pub fn kanji(&self) -> String {
+        format!(
+            "{}{}{}{}",
+            &self.0.kanji, &self.1.kanji, &self.2.kanji, &self.3.kanji
+        )
+    }
+    pub fn kana(&self) -> String {
+        format!(
+            "{}{}{}{}",
+            &self.0.reading, &self.1.reading, &self.2.reading, &self.3.reading
+        )
+    }
+}
+
 impl fmt::Display for Yojijukugo {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}{}{}{}", self.0, self.1, self.2, self.3)
@@ -45,17 +60,23 @@ impl std::str::FromStr for Yojijukugo {
     type Err = ParseYojijukugoError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut split = s.split(' ');
-        let result = split
-            .next()
-            .ok_or(ParseYojijukugoError)?
-            .chars()
-            .zip(split)
+
+        // get iterators for kanji chars, and their readings
+        let kanji_chars = split.next().ok_or(ParseYojijukugoError)?.chars();
+        let readings = split;
+
+        // zip these iterators and map into a Part struct
+        let result = kanji_chars
+            .zip(readings)
             .map(|(kanji, reading)| Part::new(kanji, reading));
 
+        // collect the kanji parts into a 4-tuple
         let (a, b, c, d) = result
             .into_iter()
             .collect_tuple()
             .ok_or(ParseYojijukugoError)?;
+
+        // construct yojijukugo from tuple
         Ok(Yojijukugo(a, b, c, d))
     }
 }
@@ -106,5 +127,14 @@ mod tests {
     #[test]
     fn can_represent() {
         assert_eq!(jigo().to_string(), "自(じ)業(ごう)自(じ)得(とく)");
+    }
+
+    #[test]
+    fn kanji_method() {
+        assert_eq!(jigo().kanji(), "自業自得");
+    }
+    #[test]
+    fn kana_method() {
+        assert_eq!(jigo().kana(), "じごうじとく");
     }
 }
